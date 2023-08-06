8bc2e4ec563f36f3a8b7c0d582a54e973eb135de
you <you@example.com>
2023-08-06 14:08:58 +0530

    changed background and removed ad looking buttons at top left

--- src/components/Start.js
+++ src/components/Start.js
@@ -7,7 +7,7 @@ function Start()
 	       <div>
 		       <div class="heading_box mt2"><h2 class="abril ">Legendary Songs</h2></div>
 				<br/>
-				<p class="playfair">Welcome to my playlist. Here we have some legendary songs and if you listen these you will realize what true songs are.</p>
+				<p class="playfair arial">Welcome to my playlist. Here we have some legendary songs and if you listen these you will realize what true songs are.</p>
 				<br/>
 			</div>
        );
